502495162c417da6e5ea2e1fbb4b5f3507c92bd9
jumbovicky <47551757+jumbovicky@users.noreply.github.com>
2022-07-24 14:33:52 +0530

    New changes
    New changes correcting the version issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-function App() {
-    return <h1>Hello from Vikram</h1>;
-};
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
 
-export default App;
+const App = () => {
+    return (
+        <Router>
+           <Link to='/'>Home</Link>
+           <Link to='/about'>About</Link>
+            <Routes>
+                <Route exact path="/" element={<Home />}/>
+                <Route path="/about" element={<About />}/>
+            </Routes>
+        </Router>
+    )
+}
+
+export default App; 
 
