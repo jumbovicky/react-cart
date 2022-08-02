d4764c0ebe1cb6babb17ec9ef2d2b635897e6322
jumbovicky <47551757+jumbovicky@users.noreply.github.com>
2022-08-03 01:23:43 +0530

    images and routes added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
+import Navigation from './components/Navigation';
 
 const App = () => {
     return (
         <Router>
-           <Link to='/'>Home</Link>
-           <Link to='/about'>About</Link>
+           <Navigation/>
            <Routes>
                 <Route exact path="/" element={<Home />}/>
                 <Route path="/about" element={<About />}/>
