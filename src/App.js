dedf0ebb2ac28484498942f802b5754d25006438
jumbovicky <47551757+jumbovicky@users.noreply.github.com>
2022-09-25 23:46:26 +0530

    Restoring original files and udpate .gitignore

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 import Navigation from './components/Navigation';
-
 const App = () => {
-    return (
+    return ( <>
         <Router>
            <Navigation/>
            <Routes>
                 <Route exact path="/" element={<Home />}/>
                 <Route path="/about" element={<About />}/>
             </Routes>
-        </Router>
-    )
+            </Router>
+        </> )
 }
 
 export default App; 
